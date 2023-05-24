7dd8543db350192bdfe3d7d72e673a9f193f866d
parul <116061472+shrikhaparul@users.noreply.github.com>
2023-05-24 12:34:22 +0530

    Update aws s3_read.py

--- common/scripts/ingestion/read/aws s3_read.py
+++ common/scripts/ingestion/read/aws s3_read.py
@@ -3,7 +3,6 @@ import logging
 import os
 import importlib
 import sys
-import csv
 import pandas as pd
 import boto3
 
@@ -14,15 +13,7 @@ decrypt = getattr(module, "decrypt")
 log2 = logging.getLogger('log2')
 ITERATION='%s iteration'
 CSV = '.csv'
-XLSX = '.xlsx'
 JSON = '.json'
-PARQUET ='.parquet'
-XML = '.xml'
-REPLACE_OPERATION= "S3 target operation is REPLACE \n target path: %s"
-APPEND_OPERATION="S3 target operation is APPEND"
-OBJECT_EXISTS ="%s object already exists in the target location"
-OBJECT_NOT_EXISTS = "%s object does not exists in the target location"
-CREATE_OBJECT = "creating new object %s"
 
 def establish_conn(json_data: dict, json_section: str,config_file_path:str):
     """establishes connection for the mysql database
@@ -30,7 +21,7 @@ def establish_conn(json_data: dict, json_section: str,config_file_path:str):
     try:
         connection_details = get_config_section(config_file_path+json_data["task"][json_section]\
         ["connection_name"]+JSON)
-        # log2.info("aws access key id %s", decrypt(connection_details["aws_access_key_id"]))
+        # log2.info("aws access key id %s", decrypt(connection_details["access_key"]))
         d_aws_access_key_id = decrypt(connection_details["access_key"])
         d_aws_secret_access_key = decrypt(connection_details["secret_access_key"])
         conn = boto3.client( service_name= 's3',region_name=
@@ -66,12 +57,11 @@ def read(json_data: dict,config_file_path,task_id,run_id,paths_data,file_path,
         sys.path.insert(0, engine_code_path)
         module = importlib.import_module("engine_code")
         audit = getattr(module, "audit")
-        conn,connection_details = establish_conn(json_data,'target',config_file_path)
+        conn,connection_details = establish_conn(json_data,'source',config_file_path)
         bucket_name = connection_details["bucket_name"]
-        # extension = '.'+json_data['task']['source']['file_type']
+        # line of code for extension = '.'+json_data['task']['source']['file_type']
         path = json_data['task']['source']['file_path']+json_data['task']['source'][
             'file_name']
-
         # List all objects in the bucket
         response = conn.list_objects_v2(Bucket=bucket_name, Prefix=path)
         objects = response['Contents']
@@ -102,44 +92,39 @@ def read(json_data: dict,config_file_path,task_id,run_id,paths_data,file_path,
             list(json_data["task"]["source"]["alias_columns"].split(","))
             default_encoding = "utf-8" if json_data["task"]["source"]["encoding"]==" " else\
             json_data["task"]["source"]["encoding"]
-
-            # file_key = json_data['task']['source']['file_path']+json_data['task']['source'][
-            #     'file_name']+'.'+json_data['task']['source']['file_type']
-
-            # # Read the file from the S3 bucket
-            # response = conn.get_object(Bucket=bucket_name, Key=file_key)
-            # content = response['Body'].read().decode('utf-8')
             for file in all_files:
                 # Call the `get_object()` method to retrieve the file object
-                file_object = conn.get_object(Bucket=bucket_name, Key=file)
-                # Count the number of records
-                record_count = sum(1 for _ in csv.reader(file_object['Body']))
-                audit(json_data, task_id,run_id,'SRC_RECORD_COUNT',record_count,
+                response = conn.get_object(Bucket=bucket_name, Key=file)
+                file_object = response['Body']
+                line_count = 0
+                # Iterate over each line in the file
+                for line in file_object.iter_lines():
+                    line_count += 1
+                # Subtract 1 if the file contains a header row
+                line_count -= 1
+                audit(json_data, task_id,run_id,'SRC_RECORD_COUNT',line_count,
                 iter_value)
-                row_count = record_count-default_skip_header-default_skip_footer
+                row_count = line_count-default_skip_header-default_skip_footer
                 count1 = 0
                 if json_data["task"]["source"]["select_columns"] != " " and \
                 json_data["task"]["source"]["alias_columns"] != " ":
                     default_header = 0
-                    # Use a `for` loop to read the file contents in chunks
-                    # for chunk in file_object['Body'].iter_chunks(chunk_size=1000):
-                    for chunk in csv.reader(file_object['Body'], names = default_alias_cols,
-                    header = default_header,engine='python',sep = default_delimiter,
-                    usecols = default_select_cols, skiprows = default_skip_header,nrows = row_count,
+                    for chunk in pd.read_csv(conn.get_object(Bucket=bucket_name, Key=file)['Body'],
+                    names = default_alias_cols,header = default_header,sep = default_delimiter,
+                    usecols = default_alias_cols,skiprows = default_skip_header,nrows = row_count,
                     chunksize = json_data["task"]["source"]["chunk_size"],
                     quotechar = default_quotechar, escapechar = default_escapechar,
                     encoding = default_encoding):
                         count1 = 1 + count1
                         log2.info(ITERATION , str(count1))
                         yield chunk
-
                 elif json_data["task"]["source"]["select_columns"] != " " and \
                 json_data["task"]["source"]["alias_columns"] == " ":
                     default_header = 'infer' if json_data["task"]["source"]["alias_columns"]\
                     == " " else 0
-                    for chunk in csv.reader(file_object['Body'], names = default_alias_cols,
-                    header = default_header,sep = default_delimiter, usecols = default_select_cols,
-                    skiprows = default_skip_header,nrows = row_count,
+                    for chunk in pd.read_csv(conn.get_object(Bucket=bucket_name, Key=file)['Body'],
+                    names = default_alias_cols,header = default_header,sep = default_delimiter,
+                    usecols = default_alias_cols,skiprows = default_skip_header,nrows = row_count,
                     chunksize = json_data["task"]["source"]["chunk_size"],
                     quotechar = default_quotechar, escapechar = default_escapechar,
                     encoding = default_encoding):
@@ -150,9 +135,9 @@ def read(json_data: dict,config_file_path,task_id,run_id,paths_data,file_path,
                 json_data["task"]["source"]["alias_columns"] != " ":
                     default_header ='infer' if json_data["task"]["source"]["alias_columns"]\
                      == " " else 0
-                    for chunk in csv.reader(file_object['Body'], names = default_alias_cols,
-                    header = default_header,sep = default_delimiter, usecols = default_alias_cols,
-                    skiprows = default_skip_header,nrows = row_count,
+                    for chunk in pd.read_csv(conn.get_object(Bucket=bucket_name, Key=file)['Body'],
+                    names = default_alias_cols,header = default_header,sep = default_delimiter,
+                    usecols = default_alias_cols,skiprows = default_skip_header,nrows = row_count,
                     chunksize = json_data["task"]["source"]["chunk_size"],
                     quotechar = default_quotechar, escapechar = default_escapechar,
                     encoding = default_encoding):
@@ -163,9 +148,10 @@ def read(json_data: dict,config_file_path,task_id,run_id,paths_data,file_path,
                 json_data["task"]["source"]["alias_columns"] == " ":
                     default_header ='infer' if json_data["task"]["source"]["alias_columns"] ==\
                      " " else None
-                    for chunk in csv.reader(file_object['Body'], names = default_alias_cols,
-                    header = default_header,sep = default_delimiter, usecols = default_select_cols,
-                    skiprows = default_skip_header,nrows = row_count,
+                    for chunk in pd.read_csv(conn.get_object(Bucket=bucket_name, Key=file)['Body'],
+                    names = default_alias_cols,header = default_header,sep = default_delimiter,
+                    usecols = default_select_cols,nrows = row_count,
+                    skiprows = default_skip_header,
                     chunksize = json_data["task"]["source"]["chunk_size"],
                     quotechar = default_quotechar, escapechar = default_escapechar,
                     encoding = default_encoding):
@@ -175,5 +161,5 @@ def read(json_data: dict,config_file_path,task_id,run_id,paths_data,file_path,
     except Exception as error:
         write_to_txt(task_id,'FAILED',file_path)
         audit(json_data, task_id,run_id,'STATUS','FAILED',iter_value)
-        log2.info("reading_csv() is %s", str(error))
+        log2.info("reading_s3() is %s", str(error))
         raise error
