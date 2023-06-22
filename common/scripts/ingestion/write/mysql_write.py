45763f8b353eb3ef5f0e4046836f0e2d407b8537
parul <116061472+shrikhaparul@users.noreply.github.com>
2023-06-22 12:05:12 +0530

    Update mysql_write.py

--- common/scripts/ingestion/write/mysql_write.py
+++ common/scripts/ingestion/write/mysql_write.py
@@ -11,169 +11,184 @@ import pymysql
 module = importlib.import_module("utility")
 get_config_section = getattr(module, "get_config_section")
 decrypt = getattr(module, "decrypt")
+module = importlib.import_module("connections")
+establish_conn_for_mysql = getattr(module, "establish_conn_for_mysql")
 
-log2 = logging.getLogger('log2')
+task_logger = logging.getLogger('task_logger')
 CURRENT_TIMESTAMP = "%Y-%m-%d %H:%M:%S"
 MYSQL_LOG_STATEMENT = "mysql ingestion completed"
 WITH_AUDIT_COLUMNS = "data ingesting with audit columns"
 WITH_OUT_AUDIT_COLUMNS = "data ingesting with out audit columns"
 
-def establish_conn(json_data: dict, json_section: str,config_file_path:str):
-    """establishes connection for the mysql database
-       you pass it through the json"""
-    try:
-        connection_details = get_config_section(config_file_path+json_data["task"][json_section]\
-        ["connection_name"]+'.json')
-        password = decrypt(connection_details["password"])
-        conn = sqlalchemy.create_engine(f'mysql+pymysql://{connection_details["username"]}'
-        f':{password.replace("@", "%40")}@{connection_details["hostname"]}'
-        f':{int(connection_details["port"])}/{connection_details["database"]}', encoding='utf-8')
-        # logging.info("connection established")
-        return conn,connection_details
-    except Exception as error:
-        log2.exception("establish_conn() is %s", str(error))
-        raise error
+# defestablish_conn(json_data: dict, json_section: str,config_file_path:str):
+#     """establishes connection for the mysql database
+#        you pass it through the json"""
+#     try:
+#         connection_details = get_config_section(config_file_path+json_data["task"][json_section]\
+#         ["connection_name"]+'.json')
+#         password = decrypt(connection_details["password"])
+#         conn = sqlalchemy.create_engine(f'mysql+pymysql://{connection_details["username"]}'
+#         f':{password.replace("@", "%40")}@{connection_details["hostname"]}'
+#         f':{int(connection_details["port"])}/{connection_details["database"]}', encoding='utf-8')
+#         # logging.info("connection established")
+#         return conn,connection_details
+#     except Exception as error:
+#         task_logger.exception("establish_conn() is %s", str(error))
+#         raise error
 
-def db_table_exists(conn: dict, tablename: str)-> bool:
+def db_table_exists(sessions: dict, tablename: str)-> bool:
     """ function for checking whether a table exists or not in mysql """
     try:
         # checking whether the table exists in database or not
         sql = f"select table_name from information_schema.tables where table_name='{tablename}'"
-        connection = conn.raw_connection()
-        cursor = connection.cursor()
-        cursor.execute(sql)
-        return bool(cursor.rowcount)
+        connection = sessions.connection()
+        result = connection.execute(sql)
+        return bool(result.rowcount)
     except Exception as error:
-        log2.exception("db_table_exists() is %s", str(error))
+        task_logger.exception("db_table_exists() is %s", str(error))
         raise error
 
-def insert_data(json_data,conn_details,dataframe,conn):
+def insert_data(json_data,conn_details,dataframe,sessions):
     """function for inserting df data in to table"""
-    if json_data["task"]["target"]["audit_columns"] == "active":
-        dataframe['CRTD_BY']=conn_details["username"]
-        dataframe['CRTD_DTTM']= datetime.now().strftime(CURRENT_TIMESTAMP)
-        dataframe['UPDT_BY']= " "
-        dataframe['UPDT_DTTM']= " "
-        log2.info(WITH_AUDIT_COLUMNS)
-        dataframe.to_sql(json_data["task"]["target"]["table_name"], conn,
-        index = False, if_exists = "append")
-        log2.info(MYSQL_LOG_STATEMENT)
-    else:
-        log2.info(WITH_OUT_AUDIT_COLUMNS)
-        dataframe.to_sql(json_data["task"]["target"]["table_name"], conn,
-        index = False, if_exists = "append")
-        log2.info(MYSQL_LOG_STATEMENT)
+    try:
+        target = json_data["task"]["target"]
+        connection = sessions.connection()
+        if target["audit_columns"] == "active":
+            dataframe['CRTD_BY']=conn_details["username"]
+            dataframe['CRTD_DTTM']= datetime.now().strftime(CURRENT_TIMESTAMP)
+            dataframe['UPDT_BY']= " "
+            dataframe['UPDT_DTTM']= " "
+            task_logger.info(WITH_AUDIT_COLUMNS)
+            dataframe.to_sql(target["table_name"], connection,
+            index = False, if_exists = "append")
+            task_logger.info(MYSQL_LOG_STATEMENT)
+        else:
+            task_logger.info(WITH_OUT_AUDIT_COLUMNS)
+            dataframe.to_sql(target["table_name"], connection,
+            index = False, if_exists = "append")
+            task_logger.info(MYSQL_LOG_STATEMENT)
+    except Exception as error:
+        # Rollback the transaction in case of an error
+        # sessions.rollback()
+        # task_logger.info("Transaction rolled back due to an error! %s", str(error))
+        task_logger.info("error occured in insert_data function %s", str(error))
+        raise error
 
 def create(json_data: dict, conn, dataframe,conn_details:str)-> bool:
     """if table is not present , it will create"""
     try:
-        if db_table_exists(conn, json_data["task"]["target"]["table_name"]) is False:
-            log2.info('%s does not exists so creating a new table',
-            json_data["task"]["target"]["table_name"])
+        target = json_data["task"]["target"]
+        if db_table_exists(conn, target["table_name"]) is False:
+            task_logger.info('%s does not exists so creating a new table',
+            target["table_name"])
             insert_data(json_data,conn_details,dataframe,conn)
         else:
-            log2.error('%s already exists, so give a new table name to create',
-            json_data["task"]["target"]["table_name"])
+            task_logger.error('%s already exists, so give a new table name to create',
+            target["table_name"])
             return False
     except OperationalError as error:
         if 'Duplicate column name' in str(error):
-            log2.error("there are duplicate column names in the target table")
+            task_logger.error("there are duplicate column names in the target table")
             sys.exit()
         else:
-            log2.info("else")
-            log2.exception("create() is %s", str(error))
+            task_logger.info("else")
+            task_logger.exception("create() is %s", str(error))
             raise error
 
 def append(json_data: dict, conn: dict, dataframe, conn_details) -> bool:
     """if table exists, it will append"""
     try:
-        if db_table_exists(conn, json_data["task"]["target"]["table_name"]) is True:
-            log2.info("%s table exists, started appending the data to table",
-            json_data["task"]["target"]["table_name"])
+        target = json_data["task"]["target"]
+        if db_table_exists(conn, target["table_name"]) is True:
+            task_logger.info("%s table exists, started appending the data to table",
+            target["table_name"])
             insert_data(json_data,conn_details,dataframe,conn)
         else:
-            log2.error('%s does not exists, so create table first',
-            json_data["task"]["target"]["table_name"])
+            task_logger.error('%s does not exists, so create table first',
+            target["table_name"])
             return False
     except OperationalError as error:
         if "Unknown column 'CRTD_BY' in 'field list'" in str(error):
-            log2.error("audit columns not found in the table previously to append")
+            task_logger.error("audit columns not found in the table previously to append")
             return False
         else:
-            log2.exception("append() is %s", str(error))
+            task_logger.exception("append() is %s", str(error))
             raise error
 
 def replace(json_data: dict, conn: dict, dataframe,counter: int, conn_details) -> bool:
     """if table exists, it will drop and replace data"""
     try:
-        if db_table_exists(conn, json_data["task"]["target"]["table_name"]) is True:
+        target = json_data["task"]["target"]
+        if db_table_exists(conn, target["table_name"]) is True:
             if counter == 1:
-                log2.info("%s table exists, started replacing the table",
-                json_data["task"]["target"]["table_name"])
+                task_logger.info("%s table exists, started replacing the table",
+                target["table_name"])
                 replace_query = sqlalchemy.text(f'DROP TABLE '
-                f'{json_data["task"]["target"]["table_name"]}')
-                conn.execution_options(autocommit=True).execute(replace_query)
-                log2.info(" table replace finished, started inserting data into "
-                 "%s table", json_data["task"]["target"]["table_name"])
+                f'{target["table_name"]}')
+                conn.execution_options(autocommit=False).execute(replace_query)
+                task_logger.info(" table replace finished, started inserting data into "
+                 "%s table", target["table_name"])
                 insert_data(json_data,conn_details,dataframe,conn)
             else:
                 insert_data(json_data,conn_details,dataframe,conn)
         else:
             # if table is not there, then it will say table does not exist
-            log2.error('%s does not exists, give correct table name',
-            json_data["task"]["target"]["table_name"])
+            task_logger.error('%s does not exists, give correct table name',
+            target["table_name"])
             return False
     except Exception as error:
-        log2.exception("replace() is %s", str(error))
+        task_logger.exception("replace() is %s", str(error))
         raise error
 
 def truncate(json_data: dict, conn: dict,dataframe,counter: int, conn_details) -> bool:
     """if table exists, it will truncate"""
     try:
-        if db_table_exists(conn, json_data["task"]["target"]["table_name"]) is True:
+        target = json_data["task"]["target"]
+        if db_table_exists(conn, target["table_name"]) is True:
             if counter == 1:
-                log2.info("%s table exists, started truncating the table",
-                json_data["task"]["target"]["table_name"])
+                task_logger.info("%s table exists, started truncating the table",
+                target["table_name"])
                 truncate_query = sqlalchemy.text(f'TRUNCATE TABLE '
-                f'{json_data["task"]["target"]["table_name"]}')
+                f'{target["table_name"]}')
                 conn.execution_options(autocommit=True).execute(truncate_query)
-                log2.info("mysql truncating table finished, started inserting data into "
-                "%s table", json_data["task"]["target"]["table_name"])
+                task_logger.info("mysql truncating table finished, started inserting data into "
+                "%s table", target["table_name"])
                 insert_data(json_data,conn_details,dataframe,conn)
             else:
                 insert_data(json_data,conn_details,dataframe,conn)
         else:
             # if table is not there, then it will say table does not exist
-            log2.error('%s does not exists, give correct table name to truncate',
-            json_data["task"]["target"]["table_name"])
+            task_logger.error('%s does not exists, give correct table name to truncate',
+            target["table_name"])
             return False
     except OperationalError as error:
         if "Unknown column 'CRTD_BY' in 'field list'" in str(error):
-            log2.error("audit columns not found in the table previously"
+            task_logger.error("audit columns not found in the table previously"
             "to insert data after truncate")
             return False
         else:
-            log2.exception("append() is %s", str(error))
+            task_logger.exception("append() is %s", str(error))
             raise error
 
 def drop(json_data: dict, conn: dict) -> bool:
     """if table exists, it will drop"""
     try:
-        if db_table_exists(conn, json_data["task"]["target"]["table_name"]) is True:
-            log2.info("%s table exists, started dropping the table",
-            json_data["task"]["target"]["table_name"])
+        target = json_data["task"]["target"]
+        if db_table_exists(conn, target["table_name"]) is True:
+            task_logger.info("%s table exists, started dropping the table",
+            target["table_name"])
             drop_query = sqlalchemy.text(f'DROP TABLE '
-            f'{json_data["task"]["target"]["table_name"]}')
+            f'{target["table_name"]}')
             conn.execution_options(autocommit=True).execute(drop_query)
-            log2.info("mysql dropping table completed")
+            task_logger.info("mysql dropping table completed")
             return True
         else:
             # if table is not there, then it will say table does not exist
-            log2.error('%s does not exists, give correct table name to drop',
-            json_data["task"]["target"]["table_name"])
+            task_logger.error('%s does not exists, give correct table name to drop',
+            target["table_name"])
             return False
     except Exception as error:
-        log2.exception("drop() is %s", str(error))
+        task_logger.exception("drop() is %s", str(error))
         raise error
 
 def write_to_txt(task_id,status,file_path):
@@ -185,70 +200,67 @@ def write_to_txt(task_id,status,file_path):
             data_fram.loc[data_fram['task_name']==task_id, 'Job_Status'] = status
             data_fram.to_csv(file_path ,mode='w', sep='\t',index = False, header=True)
         else:
-            log2.error("pipeline txt file does not exist")
+            task_logger.error("pipeline txt file does not exist")
     except Exception as error:
-        log2.exception("write_to_txt: %s.", str(error))
+        task_logger.exception("write_to_txt: %s.", str(error))
         raise error
 
-def trgt_record_count(json_data,status,conn,task_id,run_id,iter_value,audit):
+def trgt_record_count(json_data,status,sessions,task_id,run_id,iter_value,audit):
     """function to get target record count"""
     if json_data["task"]["target"]["operation"] != "drop" and status is not False:
-        connection = conn.raw_connection()
-        cursor = connection.cursor()
         sql = f'SELECT count(0) from  {json_data["task"]["target"]["table_name"]};'
-        cursor.execute(sql)
-        myresult = cursor.fetchall()
-        audit(json_data, task_id,run_id,'TRGT_RECORD_COUNT',myresult[-1][-1],
+        record_count = sessions.execute(sql).fetchall()
+        audit(json_data, task_id,run_id,'TRGT_RECORD_COUNT',record_count[-1][-1],
         iter_value)
-        log2.info('the number of records present in target table after ingestion:%s',
-        myresult[-1][-1])
-
-
+        task_logger.info('the number of records present in target table after ingestion:%s',
+        record_count[-1][-1])
 
 def write(json_data,datafram,counter,config_file_path,task_id,run_id,paths_data,
-          file_path,iter_value) -> bool:
+          file_path,iter_value,sessions) -> bool:
     """ function for ingesting data to mysql based on the operation in json"""
     try:
-        engine_code_path = paths_data["folder_path"]+paths_data["ingestion_path"]
+        target = json_data["task"]["target"]
+        engine_code_path = os.path.expanduser(paths_data["folder_path"])+paths_data[
+            "ingestion_path"]
         sys.path.insert(0, engine_code_path)
         #importing audit function from engine_code script
         module1 = importlib.import_module("engine_code")
         audit = getattr(module1, "audit")
-        log2.info("ingest data to mysql db initiated")
-        conn,conn_details = establish_conn(json_data,'target',config_file_path)
+        task_logger.info("ingest data to mysql db initiated")
+        _ ,conn_details = establish_conn_for_mysql(json_data,'target',
+                                                     config_file_path)
         status="Pass"
-        if json_data["task"]["target"]["operation"] == "create":
+        if target["operation"] == "create":
             if counter == 1:
-                status=create(json_data, conn, datafram, conn_details)
+                status=create(json_data, sessions, datafram, conn_details)
             else:
-                status=append(json_data, conn, datafram,conn_details)
-        elif json_data["task"]["target"]["operation"] == "append":
-            status=append(json_data, conn, datafram,conn_details)
-        elif json_data["task"]["target"]["operation"] == "truncate":
-            status=truncate(json_data, conn, datafram, counter,conn_details)
-        elif json_data["task"]["target"]["operation"] == "drop":
-            status=drop(json_data, conn)
-        elif json_data["task"]["target"]["operation"] == "replace":
-            status=replace(json_data, conn, datafram, counter,conn_details)
-        elif json_data["task"]["target"]["operation"] not in ("create", "append",
+                status=append(json_data, sessions, datafram,conn_details)
+        elif target["operation"] == "append":
+            status=append(json_data, sessions, datafram,conn_details)
+        elif target["operation"] == "truncate":
+            status=truncate(json_data, sessions, datafram, counter,conn_details)
+        elif target["operation"] == "drop":
+            status=drop(json_data, sessions)
+        elif target["operation"] == "replace":
+            status=replace(json_data, sessions, datafram, counter,conn_details)
+        elif target["operation"] not in ("create", "append",
             "truncate", "drop","replace"):
-            log2.error("give propper input for operation condition")
+            task_logger.error("give propper input for operation condition")
             status = False
-        trgt_record_count(json_data,status,conn,task_id,run_id,iter_value,audit)
-        conn.dispose()
+        trgt_record_count(json_data,status,sessions,task_id,run_id,iter_value,audit)
         return status
     except OperationalError:
-        # log2.error("there are duplicate column names in the target table")
+        # task_logger.error("there are duplicate column names in the target table")
         write_to_txt(task_id,'FAILED',file_path)
         audit(json_data, task_id,run_id,'STATUS','FAILED',iter_value)
         sys.exit()
     except pymysql.err.ProgrammingError: #to handle table not found issue
-        log2.error("the table name or connection specified in the task is incorrect")
+        task_logger.error("the table name or connection specified in the task is incorrect")
         write_to_txt(task_id,'FAILED',file_path)
         audit(json_data, task_id,run_id,'STATUS','FAILED',iter_value)
         sys.exit()
     except Exception as error:
         write_to_txt(task_id,'FAILED',file_path)
         audit(json_data, task_id,run_id,'STATUS','FAILED',iter_value)
-        log2.exception("write() is %s", str(error))
+        task_logger.exception("write() is %s", str(error))
         raise error
