2087a3513cef2470d7e9a81c3b5fca5e0b51adbc
parul <116061472+shrikhaparul@users.noreply.github.com>
2023-06-22 12:04:37 +0530

    Update csv_write.py

--- common/scripts/ingestion/write/csv_write.py
+++ common/scripts/ingestion/write/csv_write.py
@@ -4,61 +4,54 @@ from datetime import datetime
 import os
 from ast import literal_eval
 
-log2 = logging.getLogger('log2')
+Task_Logger = logging.getLogger('Task_Logger')
 
 def write(json_data: dict,datafram, counter) -> bool:
     """ function for writing data to csv file"""
     try:
-        log2.info("writing data to csv file")
+        target = json_data["task"]["target"]
+        Task_Logger.info("writing data to csv file")
         if counter ==1: # for first iteration
-            if json_data["task"]["target"]["audit_columns"] == "active":
+            if target["audit_columns"] == "active":
                 # if audit_columns are active
                 datafram['CRTD_BY']="etl_user"
                 datafram['CRTD_DTTM']= datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 datafram['UPDT_BY']= " "
                 datafram['UPDT_DTTM']= " "
-                if os.path.exists(json_data["task"]["target"]["file_path"]+
-                json_data["task"]["target"]["file_name"]):
-                    os.remove(json_data["task"]["target"]["file_path"]+
-                    json_data["task"]["target"]["file_name"])
-                datafram.to_csv(json_data["task"]["target"]["file_path"]+
-                json_data["task"]["target"]["file_name"],
-                sep=json_data["task"]["target"]["delimiter"], header=literal_eval(
-                json_data["task"]["target"]["header"]),
-                index=literal_eval(json_data["task"]["target"]["index"]), mode='a',
-                encoding=json_data["task"]["target"]["encoding"])
+                if os.path.exists(target["file_path"]+target["file_name"]):
+                    os.remove(target["file_path"]+target["file_name"])
+                datafram.to_csv(target["file_path"]+target["file_name"],
+                sep=target["delimiter"], header=literal_eval(
+                target["header"]),
+                index=literal_eval(target["index"]), mode='a',
+                encoding=target["encoding"])
             else:
                 # if audit_columns are  not active
-                if os.path.exists(json_data["task"]["target"]["file_path"]+
-                json_data["task"]["target"]["file_name"]):
-                    os.remove(json_data["task"]["target"]["file_path"]+
-                    json_data["task"]["target"]["file_name"])
-                datafram.to_csv(json_data["task"]["target"]["file_path"]+
-                json_data["task"]["target"]["file_name"],
-                sep=json_data["task"]["target"]["delimiter"], header=literal_eval(
-                json_data["task"]["target"]["header"]),
-                index=literal_eval(json_data["task"]["target"]["index"]), mode='a',
-                encoding=json_data["task"]["target"]["encoding"])
+                if os.path.exists(target["file_path"]+target["file_name"]):
+                    os.remove(target["file_path"]+target["file_name"])
+                datafram.to_csv(target["file_path"]+target["file_name"],
+                sep=target["delimiter"], header=literal_eval(
+                target["header"]),
+                index=literal_eval(target["index"]), mode='a',
+                encoding=target["encoding"])
         else: # for iterations other than one
-            if json_data["task"]["target"]["audit_columns"] == "active":
+            if target["audit_columns"] == "active":
                 # if audit_columns are active
                 datafram['CRTD_BY']="etl_user"
                 datafram['CRTD_DTTM']= datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 datafram['UPDT_BY']= " "
                 datafram['UPDT_DTTM']= " "
-                datafram.to_csv(json_data["task"]["target"]["file_path"]+
-                json_data["task"]["target"]["file_name"],
-                sep=json_data["task"]["target"]["delimiter"], header=False,
-                index=literal_eval(json_data["task"]["target"]["index"]),
-                mode='a', encoding=json_data["task"]["target"]["encoding"])
+                datafram.to_csv(target["file_path"]+target["file_name"],
+                sep=target["delimiter"], header=False,
+                index=literal_eval(target["index"]),
+                mode='a', encoding=target["encoding"])
             else:
                 # if audit_columns are  not active
-                datafram.to_csv(json_data["task"]["target"]["file_path"]+
-                json_data["task"]["target"]["file_name"],
-                sep=json_data["task"]["target"]["delimiter"], header=False,
-                index=literal_eval(json_data["task"]["target"]["index"]),
-                mode='a', encoding=json_data["task"]["target"]["encoding"])
+                datafram.to_csv(target["file_path"]+target["file_name"],
+                sep=target["delimiter"], header=False,
+                index=literal_eval(target["index"]),
+                mode='a', encoding=target["encoding"])
         return True
     except Exception as error:
-        log2.exception("ingest_data_to_csv() is %s", str(error))
+        Task_Logger.exception("ingest_data_to_csv() is %s", str(error))
         raise error
