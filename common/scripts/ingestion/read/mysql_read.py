b7aeb8962fae1ada27193e48c7ac136e7dc17e9e
parul <116061472+shrikhaparul@users.noreply.github.com>
2023-06-22 11:57:13 +0530

    Update mysql_read.py

--- common/scripts/ingestion/read/mysql_read.py
+++ common/scripts/ingestion/read/mysql_read.py
@@ -3,99 +3,86 @@ import logging
 import os
 import sys
 import importlib
-import sqlalchemy
 import pymysql
 import pandas as pd
 
 module = importlib.import_module("utility")
 get_config_section = getattr(module, "get_config_section")
 decrypt = getattr(module, "decrypt")
+module = importlib.import_module("connections")
+establish_conn_for_mysql = getattr(module, "establish_conn_for_mysql")
 
-
-log2 = logging.getLogger('log2')
-
-def establish_conn(json_data: dict, json_section: str,config_file_path:str):
-    """establishes connection for the mysql database
-       you pass it through the json"""
-    try:
-        connection_details = get_config_section(config_file_path+json_data["task"][json_section]
-        ["connection_name"]+'.json')
-        password = decrypt(connection_details["password"])
-        conn1 = sqlalchemy.create_engine(f'mysql+pymysql://{connection_details["username"]}'
-        f':{password.replace("@", "%40")}@{connection_details["hostname"]}'
-        f':{int(connection_details["port"])}/{connection_details["database"]}', encoding='utf-8')
-        # logging.info("connection established")
-        return conn1
-    except Exception as error:
-        log2.exception("establish_conn() is %s", str(error))
-        raise error
+task_logger = logging.getLogger('task_logger')
 
 def write_to_txt(task_id,status,file_path):
     """Generates a text file with statuses for orchestration"""
     try:
         is_exist = os.path.exists(file_path)
         if is_exist is True:
-            # log2.info("txt getting called")
+            # task_logger.info("txt getting called")
             data_fram =  pd.read_csv(file_path, sep='\t')
             data_fram.loc[data_fram['task_name']==task_id, 'Job_Status'] = status
             data_fram.to_csv(file_path ,mode='w', sep='\t',index = False, header=True)
         else:
-            log2.error("pipeline txt file does not exist")
+            task_logger.error("pipeline txt file does not exist")
     except Exception as error:
-        log2.exception("write_to_txt: %s.", str(error))
+        task_logger.exception("write_to_txt: %s.", str(error))
         raise error
 
 def read(json_data,config_file_path,task_id,run_id,paths_data,file_path,iter_value):
     """ function for reading data from mysql table"""
     try:
-        engine_code_path = paths_data["folder_path"]+paths_data["ingestion_path"]
+        engine_code_path = os.path.expanduser(paths_data["folder_path"])+paths_data[
+            "ingestion_path"]
         sys.path.insert(0, engine_code_path)
         #importing audit function from orchestrate script
         module1 = importlib.import_module("engine_code")
         audit = getattr(module1, "audit")
-        conn3 = establish_conn(json_data, 'source',config_file_path)
-        log2.info('reading data from mysql started')
+        conn3,_ = establish_conn_for_mysql(json_data, 'source',config_file_path)
+        task_logger.info('reading data from mysql started')
         connection = conn3.raw_connection()
         cursor = connection.cursor()
+        source = json_data["task"]["source"]
         count1 = 0
-        if json_data["task"]["source"]["query"] == " ":
-            log2.info("reading from mysql table: %s",
-            json_data["task"]["source"]["table_name"])
-            sql = f'SELECT count(0) from {json_data["task"]["source"]["table_name"]};'
+        if source["query"] == " ":
+            task_logger.info("reading from mysql table: %s",
+            source["table_name"])
+            sql = f'SELECT count(0) from {source["table_name"]};'
             cursor.execute(sql)
             myresult = cursor.fetchall()
             audit(json_data, task_id,run_id,'SRC_RECORD_COUNT',myresult[-1][-1],
             iter_value)
-            log2.info('the number of records present in source table before ingestion:%s',
+            task_logger.info('the number of records present in source table before ingestion:%s',
             myresult[-1][-1])
-            default_columns = None if json_data["task"]["source"]["select_columns"]==" "\
-            else list(json_data["task"]["source"]["select_columns"].split(","))
-            for query in pd.read_sql_table(json_data["task"]["source"]["table_name"], conn3,\
+            default_columns = None if source["select_columns"]==" "\
+            else list(source["select_columns"].split(","))
+            for query in pd.read_sql_table(source["table_name"], conn3,\
             columns = default_columns, \
-            chunksize = json_data["task"]["source"]["chunk_size"]):
+            chunksize = source["chunk_size"]):
                 count1+=1
-                log2.info('%s iteration' , str(count1))
+                task_logger.info('%s iteration' , str(count1))
                 yield query
         else:
-            log2.info("reading from sql query")
-            sql = f'SELECT count(0) from ({json_data["task"]["source"]["query"]}) as d;'
-            log2.info(sql)
+            task_logger.info("reading from sql query")
+            sql = f'SELECT count(0) from ({source["query"]}) as d;'
+            task_logger.info(sql)
             cursor.execute(sql)
             myresult = cursor.fetchall()
             audit(json_data, task_id,run_id,'SRC_RECORD_COUNT',myresult[-1][-1],
             iter_value)
-            log2.info('the number of records present in source table before ingestion:%s',
+            task_logger.info('the number of records present in source table before ingestion:%s',
             myresult[-1][-1])
-            log2.info('sql_query: %s',json_data["task"]["source"]["query"])
-            for query in pd.read_sql(json_data["task"]["source"]["query"],
-            conn3, chunksize = json_data["task"]["source"]["chunk_size"]):
+            task_logger.info('sql_query: %s',source["query"])
+            for query in pd.read_sql(source["query"],
+            conn3, chunksize = source["chunk_size"]):
                 count1+=1
-                log2.info('%s iteration' , str(count1))
+                task_logger.info('%s iteration' , str(count1))
                 yield query
         conn3.dispose()
         return True
     except pymysql.err.ProgrammingError: #to handle table not found issue
-        log2.error("the table name or connection specified in the task is incorrect/doesnot exists")
+        task_logger.error("the table name or connection specified in the  \
+                          task is incorrect/doesnot exists")
         write_to_txt(task_id,'FAILED',file_path)
         audit(json_data, task_id,run_id,'STATUS','FAILED',
         iter_value)
@@ -103,5 +90,5 @@ def read(json_data,config_file_path,task_id,run_id,paths_data,file_path,iter_val
     except Exception as error:
         write_to_txt(task_id,'FAILED',file_path)
         audit(json_data, task_id,run_id,'STATUS','FAILED',iter_value)
-        log2.exception("read_data_from_mysql() is %s", str(error))
+        task_logger.exception("read_data_from_mysql() is %s", str(error))
         raise error
